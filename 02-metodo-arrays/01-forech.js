4290cdcf1bfda7a58c2159cb42570fae8326f012
evellyn <evellyn.aquino@edu.senai.br>
2026-08-18 13:57:50 -0300

    salvando

--- 02-metodo-arrays/01-forech.js
+++ 02-metodo-arrays/01-forech.js
@@ -1,5 +1,9 @@
 const frutas = ["Maça", "banana", "Uva"]
 
-for (let i = 1; i < frutas.length; i++){
+for (let i = 0; i < frutas.length; i++){
     console.log(`${frutas[i]}`)
 }
+
+frutas.forEach((a) => {
+    console.log(a);
+});
---
+++ 02-metodo-arrays/02-map.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const dobrado = numeros.map((num) => num * 2);
+
+console.log(dobrado);
+
+let nomes = ["Evellyn", "yuri", "victoria", "matheus"];
+
+let mensagem = nomes.map((nome) => `Olá, ${nome}`);
+
+console.log(mensagem);
